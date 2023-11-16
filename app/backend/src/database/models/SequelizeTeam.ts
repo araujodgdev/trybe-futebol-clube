4f88a26be382cf9024006d433629866f9edc8071
Douglas Araújo <devdoug45@gmail.com>
2023-11-16 12:19:01 +0000

    feat: cria o sequelize model de teams

---
+++ app/backend/src/database/models/SequelizeTeam.ts
@@ -0,0 +1,39 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class SequelizeTeam extends Model<
+InferAttributes<SequelizeTeam>,
+InferCreationAttributes<SequelizeTeam>
+> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+SequelizeTeam.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    teamName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize: db,
+    tableName: 'teams',
+    timestamps: false,
+    underscored: true,
+  },
+);
+
+export default SequelizeTeam;
